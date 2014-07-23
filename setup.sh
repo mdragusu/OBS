9849e3539061bb2a6f9af9858a92292ef019f79f
Mihai Dragusu <mihai.dragusu@windriver.com>
2014-07-23 11:21:56 +0300

    add setup.sh script
    Signed-off-by: Mihai Dragusu <mihai.dragusu@windriver.com>

---
+++ setup.sh
@@ -0,0 +1,16 @@
+sudo apt-get install lightdm \
+                     openbox \
+                     lxappearance \
+                     lxrandr \
+                     obconf \
+                     feh \
+                     cmus \
+                     xfce4-panel \
+                     conky \
+                     pavucontrol \
+                     xfce4-terminal
+
+cp -R ./config ~/
+cp -R ./themes ~/
+cp -R ./icons ~/
+cp b.jpg ~/Pictures
