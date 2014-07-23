9bdb953363af6fa01767d867867af5e4d3034e34
Mihai Dragusu <mdragusu@gmail.com>
2014-07-23 12:46:27 +0300

    update setup.sh
    Signed-off-by: Mihai Dragusu <mdragusu@gmail.com>

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ sudo apt-get install lightdm \
                      pavucontrol \
                      xfce4-terminal \
                      gmrun \
+echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d
 cp -R ./config ~/
 cp -R ./themes ~/
 cp -R ./icons ~/
