7fd75caabe1893cb7eea6ec07758f484acf4158e
Mihai Dragusu <mdragusu@gmail.com>
2014-07-23 22:43:11 +0300

    update setup
    Signed-off-by: Mihai Dragusu <mdragusu@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
-sudo apt-get install lightdm \
-                     openbox \
-                     lxappearance \
-                     lxrandr \
-                     obconf \
-                     feh \
-                     cmus \
-                     xfce4-panel \
-                     conky \
-                     pavucontrol \
-                     xfce4-terminal \
-                     gmrun \
-echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d
-cp -R ./config ~/
-cp -R ./themes ~/
-cp -R ./icons ~/
-cp b.jpg ~/Pictures
+sudo apt-get install \
+lightdm \
+openbox \
+lxappearance \
+lxrandr \
+obconf \
+feh \
+cmus \
+xfce4-panel \
+conky \
+pavucontrol \
+xfce4-terminal \
+gmrun \
+nemo \
+htop
+
+echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
+gsettings set org.nemo.desktop show-desktop-icons false
+cp -R ./.config ~/
+cp -R ./.themes ~/
