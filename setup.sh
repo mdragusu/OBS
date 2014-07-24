a49056fd9059a27bdca2a3458e9511bb92b9a507
Mihai Dragusu <mdragusu@gmail.com>
2014-07-24 10:08:09 +0300

    update setup and terminal config
    Signed-off-by: Mihai Dragusu <mdragusu@gmail.com>

--- setup.sh
+++ setup.sh
@@ -13,9 +13,10 @@ xfce4-terminal \
 gmrun \
 nemo \
 htop \
-elementary-icon-theme
-
-echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
+elementary-icon-theme \
+xfonts-terminus \
+console-terminus
+#echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 gsettings set org.nemo.desktop show-desktop-icons false
 git clone https://github.com/nsf/obkey.git
 cp -R ./.config ~/
