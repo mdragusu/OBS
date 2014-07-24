9a722f1ae0ad96c4ff8b9bb8f3041f801b0fcd82
Mihai Dragusu <mihai.dragusu@windriver.com>
2014-07-24 09:46:16 +0300

    update setup
    Signed-off-by: Mihai Dragusu <mihai.dragusu@windriver.com>

--- setup.sh
+++ setup.sh
@@ -13,9 +13,10 @@ xfce4-terminal \
 gmrun \
 nemo \
 htop \
-elementary-icon-theme \
+elementary-icon-theme
 
 echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 gsettings set org.nemo.desktop show-desktop-icons false
+git clone https://github.com/nsf/obkey.git
 cp -R ./.config ~/
 cp -R ./.themes ~/
