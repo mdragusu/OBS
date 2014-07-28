0ba3facf1a3dc223dc1b5dd0b07282da02ce8871
Mihai Dragusu <mihai.dragusu@windriver.com>
2014-07-28 09:36:30 +0300

    update settings and setup
    Signed-off-by: Mihai Dragusu <mihai.dragusu@windriver.com>

--- setup.sh
+++ setup.sh
@@ -7,20 +7,23 @@ obconf \
 feh \
 xfce4-panel \
 xfce4-terminal \
+xfce4-indicator-plugin \
+xfce4-whiskermenu-plugin \
 pavucontrol \
 gmrun \
-nemo \
+pcmanfm \
 htop \
 light-locker \
 xfce4-mixer \
 conky \
 elementary-icon-theme \
 xfonts-terminus \
-console-terminus
-
+console-terminus \
+ttf-ubuntu-font-family 
 
 #echo "user-session=openbox" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
-gsettings set org.nemo.desktop show-desktop-icons false
-git clone https://github.com/nsf/obkey.git
+#gsettings set org.nemo.desktop show-desktop-icons false
+#git clone https://github.com/nsf/obkey.git
+#git clone https://github.com/djmelik/archey.git
 cp -R ./.config ~/
 cp -R ./.themes ~/
